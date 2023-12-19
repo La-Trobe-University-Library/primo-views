7a0581a2fe0276a846c539862fd55fb2ee595846
mjsier <m.sier@latrobe.edu.au>
2023-12-19 14:50:27 +1100

    Added base sandbox files
    Added current configuration files from sandbox Primo 19/12/2023

---
+++ LATROBE/js/custom.js
@@ -0,0 +1,150 @@
+// Updated custom.js from Third Iron to fix the BrowZine integration errors - 2023-11-30 v3
+// Changed unpaywallEmailAddressKey, changed the single quote marks in prmCitationAfter
+// Mimecast replaced all the URLs! Fixed them.
+// Note that Third Iron removed all the comments from this file. I'll put them back if this works.
+
+(function() {
+    "use strict";
+    'use strict';
+	
+	
+	// Talis reading list integration, based on the https://github.com/alfi1/primo-aspire-api
+	
+    var app = angular.module('viewCustom', ['angularLoad']).constant('AspireTrustBaseUrl', "https://latrobe.rl.talis.com/").config(['$sceDelegateProvider', 'AspireTrustBaseUrl', function($sceDelegateProvider, AspireTrustBaseUrl) {
+        var urlWhitelist = $sceDelegateProvider.resourceUrlWhitelist();
+        urlWhitelist.push(AspireTrustBaseUrl + '**');
+        $sceDelegateProvider.resourceUrlWhitelist(urlWhitelist);
+    }]);
+	
+    app.component('prmServiceDetailsAfter', {
+        bindings: {
+            parentCtrl: '<'
+        },
+        controller: 'DisplayAspireListsController',
+        template: '<div ng-if="listsFound"><span class="bold text">Cited on reading lists:</span><ul><li ng-repeat="(url,listname) in listsFound"><a href="{{url}}">{{listname}} </a></li></ul></div>'
+    });
+	
+    app.controller('DisplayAspireListsController', function($scope, $http) {
+        var vmt = this;
+		
+        this.$onInit = function() {
+            {
+                var theMMSID = vmt.parentCtrl.item.pnx.search.addsrcrecordid[0];
+                console.log("LY: function returns " + theMMSID);
+                
+				var url = 'https://latrobe.rl.talis.com/lcn/' + theMMSID + '/lists.json';
+				
+                $http.jsonp(url, {
+                    jsonpCallbackParam: 'cb'
+                }).then(function handleSuccess(response) {
+                    $scope.listsFound = response.data;
+                });
+            }
+        };
+    });
+	//end Talis reading list integration
+
+    //LibChat integration
+    var libchatHash = '18578295f317837477f054d32b1e7b01';
+    var div = document.createElement('div');
+    div.id = 'libchat_' + libchatHash;
+    document.getElementsByTagName('body')[0].appendChild(div);
+    var scr = document.createElement('script');
+    scr.src = 'https://latrobe.libanswers.com/load_chat.php? hash=' + libchatHash;
+    setTimeout(function() {
+        document.getElementsByTagName('body')[0].appendChild(scr);
+    }, 2000);
+	//end LibChat integration
+
+    //BrowZine integration
+    var app = angular.module('viewCustom', ['chat'])
+    angular.module('chat', ['angularLoad']).component('addChat', {
+        controller: ['angularLoad', function(angularLoad) {
+            this.$onInit = function() {
+                angularLoad.loadScript('https://latrobe.libanswers.com/load_chat.php?hash=16f4a310dcd007c4fff853f28ece1970')
+            }
+        }]
+    })
+    app.component('prmTopBarBefore', {
+        template: '<add-chat />'
+    })
+
+    
+    app.controller('prmLogoAfterController', [function() {
+        var vm = this;
+        vm.getIconLink = getIconLink;
+        function getIconLink() {
+            return vm.parentCtrl.iconLink;
+        }
+    }]);
+    app.component('prmLogoAfter', {
+        bindings: {
+            parentCtrl: '<'
+        },
+        controller: 'prmLogoAfterController',
+        template: '<div class="product-logo product-logo-local" layout="row" layout-align="start center" layout-fill id="banner" tabindex="0" role="banner"><img src="custom/LATROBE/img/library-logo.png" alt="La Trobe University Library" usemap="#logomap_lg" class="logo-image"><map name="logomap_lg"><area shape="rect" coords="0,0,135,48" alt="La Trobe" href="http://latrobe.edu.au"><area shape="rect" coords="0,0,210,48" alt="Library" href="http://latrobe.edu.au/library"></map></div>'
+    });
+	
+	// Add EasyCite link after Citations area
+	/*Thanks to Amelia Rowe, RMIT 09/08/2019 . Added 20/09/21 Rachel S Discovery LTU*/
+    app.component('prmCitationAfter', {
+        bindings: {
+            parentCtrl: '<'
+        },
+        template: '<div class="layout-align-center-center layout-row" layout-align="center center" style="background-color: #ede49e; padding-top: 0.5em;" ><span class="bar-text"><p style="font-size: medium;font-family: arial;font-weight: bold;">Ensure your Citation meets La Trobe standards via our <a target=_blank href=https://latrobe.libguides.com/academicreftoolgroup target="_blank">Academic Referencing Tool</a></p></span></div>'
+    });
+
+    
+  window.browzine = {
+  
+    libraryId: "889",
+    apiKey: "a8d9b1e9-fda2-4043-9e64-08f7bbc85754",
+    
+    journalCoverImagesEnabled: true,
+    
+    journalBrowZineWebLinkTextEnabled: true,
+    journalBrowZineWebLinkText: "View Journal Contents",
+    
+    articleBrowZineWebLinkTextEnabled: true,
+    articleBrowZineWebLinkText: "View Issue Contents",
+    
+    articlePDFDownloadLinkEnabled: true,
+    articlePDFDownloadLinkText: "Download PDF",
+    
+    articleLinkEnabled: true,
+    articleLinkText: "Read Article",
+    
+    printRecordsIntegrationEnabled: true,
+    showFormatChoice: false,
+    showLinkResolverLink: true,
+    enableLinkOptimizer: true,
+  
+    articleRetractionWatchEnabled: true,
+    articleRetractionWatchText: "Retracted Article",
+    
+    unpaywallEmailAddressKey: "d.witteveen@latrobe.edu.au",
+    articlePDFDownloadViaUnpaywallEnabled: true,
+    articlePDFDownloadViaUnpaywallText: "Download PDF (via Unpaywall)",
+    articleLinkViaUnpaywallEnabled: true,
+    articleLinkViaUnpaywallText: "Read Article (via Unpaywall)",
+    articleAcceptedManuscriptPDFViaUnpaywallEnabled: true,
+    articleAcceptedManuscriptPDFViaUnpaywallText: "Download PDF (Accepted Manuscript via Unpaywall)",
+    articleAcceptedManuscriptArticleLinkViaUnpaywallEnabled: true,
+    articleAcceptedManuscriptArticleLinkViaUnpaywallText: "Read Article (Accepted Manuscript via Unpaywall)",
+  };
+
+  browzine.script = document.createElement("script");
+  browzine.script.src = "https://s3.amazonaws.com/browzine-adapters/primo/browzine-primo-adapter.js";
+  document.head.appendChild(browzine.script);
+
+  app.controller('prmSearchResultAvailabilityLineAfterController', function($scope) {
+    window.browzine.primo.searchResult($scope);
+  });
+
+  app.component('prmSearchResultAvailabilityLineAfter', {
+    bindings: { parentCtrl: '<' },
+    controller: 'prmSearchResultAvailabilityLineAfterController'
+  });
+// ... End BrowZine - Primo Integration
+
+})();
--- README.md
+++ README.md
@@ -1,18 +1,71 @@
 # primo-views
 
 ## Description 
+This repository holds the [Primo Customization Packages](https://knowledge.exlibrisgroup.com/Primo/Product_Documentation/Primo/Back_Office_Guide/090Primo_Utilities/The_UI_Customization_Package_Manager) for La Trobe University Library's Production and Sandbox systems.
 
-For tracking changes to Primo view packages
+The repository contains two permanent branches:
 
-## Copyright
+- **production** holds the current files for the Primo Production system
+- **sandbox** holds the current files for the Primo Sandbox system
+
+Both branches store the extracted files (rather than the zipped-up packages) so we can use version comparison tools on them.
+
+
+## Updating Files
+
+If you want to make changes, make a "work in progress" branch rather than editing the sandbox or production branches directly.
+
+1. Make a WIP branch from the **sandbox** branch
+2. Make your changes. Test them in Sandbox. When they work...
+3. Merge your WIP branch into the **sandbox** branch
+
+We may be making multiple changes to Sandbox before we're ready to deploy them to Production.
+
+When we're happy with the **sandbox** branch...
+1. Deploy your changes to Primo Production
+2. Merge the sandbox branch into the **production** branch. DO NOT DELETE THE SANBOX BRANCH!
+
+
+### Detailed instructions
+1. Preparation
+    1. Create your own GitHub account to access repositories
+    2. Install GitHub Desktop
+
+2. Create your work-in-pogress branch:
+    1. Create a new branch
+    2. Clone a repository to your desktop: File > Clone Respository...
+    3. Create a branch of the repository: Branch > New Branch...
+    4. Give this new branch a descriptive name (e.g. Fixing_Talis_Integration or Updating_Embedded_Video)
+
+3. Make all your changes there. 
+    1. Upload to Primo. Test them. 
+    2. When they work...
+
+4. Save your changes
+    1. Save and commit any changes to your local branch: Changes side tab > Commit to <branchname>
+    2. Push your local commits to the remote repository: Push origin
+    3. Publish your current branch to GitHub.
+
+5. Merge your changes into the original branch
+    1. Create a pull request
+    2. Invite reviewers
+    3. Address their review comments
+    4. Merge your pull request
+
+6. Delete your branch from both GitHub and your local machine
+
+
+## Misc
+
+### Copyright
 
 Other than any prior works of which it is a derivative, the copyright in this work is owned by La Trobe University.
 
-## Licenses
+### Licenses
 
 Rights of use and distribution are granted under the terms of the GNU Affero General Public License version 3 (AGPL-3.0). You should find a copy of this license in the root of the repository.
 
-## Contact
+### Contact
 
 The maintainer of this repository is Hugh Rundle, who can be contacted at h.rundle@latrobe.edu.au
 
