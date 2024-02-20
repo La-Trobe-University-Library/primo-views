39d183203dd6e805e56a08bd2521c955ac981da7
ShannonH-LTU <157654319+ShannonH-LTU@users.noreply.github.com>
2024-02-20 11:57:25 +1100

    Added La Trobe fonts
    Added and applied the La Trobe fonts ('Knockout' and 'DM sans').
    
    Minor CSS updates.

--- LATROBE/js/custom.js
+++ LATROBE/js/custom.js
@@ -4,7 +4,7 @@
 
 var app = angular.module('viewCustom', ['angularLoad']);
 
-console.log('LATROBE (Branding) JS version 0.1.13');
+console.log('LATROBE (Branding) JS version 0.1.14');
 //console.log('includes: LibChat, Browzine, Talis');
 
 /* -------------------------------------------
