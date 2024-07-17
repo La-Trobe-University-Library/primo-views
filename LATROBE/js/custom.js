72ffd7c1995a7d9be2108e70104a79cca577dc53
ShannonH-LTU <157654319+ShannonH-LTU@users.noreply.github.com>
2024-07-17 14:53:31 +1000

    Style update
    CSS for the login dialog (for 'Other borrower') has been added.
    
    The 'CATONLYPC' view has been updated to match the 'LATROBE' view.

--- LATROBE/js/custom.js
+++ LATROBE/js/custom.js
@@ -385,7 +385,7 @@
     controller: 'GuidedTourController',
     template: 
       '<a id="tour_button" href="" ng-show="tourLabel" ng-click="startTour()" ng-class="{\'animate\':animateButton, \'show\':tourLabel}">'+
-        '<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" style="margin: 0 5px 0 0;font-size: 1.1em;"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M224 32H64C46.3 32 32 46.3 32 64v64c0 17.7 14.3 32 32 32H441.4c4.2 0 8.3-1.7 11.3-4.7l48-48c6.2-6.2 6.2-16.4 0-22.6l-48-48c-3-3-7.1-4.7-11.3-4.7H288c0-17.7-14.3-32-32-32s-32 14.3-32 32zM480 256c0-17.7-14.3-32-32-32H288V192H224v32H70.6c-4.2 0-8.3 1.7-11.3 4.7l-48 48c-6.2 6.2-6.2 16.4 0 22.6l48 48c3 3 7.1 4.7 11.3 4.7H448c17.7 0 32-14.3 32-32V256zM288 480V384H224v96c0 17.7 14.3 32 32 32s32-14.3 32-32z"></path></svg>'+
+        '<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" style="margin: 0 5px 0 0;font-size: 1.1em;max-width: 18px;"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M224 32H64C46.3 32 32 46.3 32 64v64c0 17.7 14.3 32 32 32H441.4c4.2 0 8.3-1.7 11.3-4.7l48-48c6.2-6.2 6.2-16.4 0-22.6l-48-48c-3-3-7.1-4.7-11.3-4.7H288c0-17.7-14.3-32-32-32s-32 14.3-32 32zM480 256c0-17.7-14.3-32-32-32H288V192H224v32H70.6c-4.2 0-8.3 1.7-11.3 4.7l-48 48c-6.2 6.2-6.2 16.4 0 22.6l48 48c3 3 7.1 4.7 11.3 4.7H448c17.7 0 32-14.3 32-32V256zM288 480V384H224v96c0 17.7 14.3 32 32 32s32-14.3 32-32z"></path></svg>'+
         '<span ng-bind-html="tourLabel"></span>'+
       '</a>'
   });
@@ -731,15 +731,15 @@
                 align: "center"
               }
             }, {
-              element: "#select_21",
+              element: "prm-pre-filters > div > md-input-container:nth-child(1) md-select",
               popover: {
                 title: "Resource type",
                 description: "<p>You can specify the type of resource you're searching for using this drop-down. You will also be able to apply additional filters after performing a search.</p>",
                 side: "bottom",
-                align: "center"
+                align: "left"
               }
             }, {
-              element: "#select_35",
+              element: "prm-pre-filters > div > md-input-container:nth-child(2) md-select",
               popover: {
                 title: "Search field",
                 description: "<p>If you want to search only within a specific field, you can specify it in this drop-down. Leave it as 'anywhere in the record' to broaden your search.</p>",
